b3e07ddfc91baf5ac649120b3a1a79464ae78fc2
Your Name <example@example.com>
2025-05-19 20:29:56 -0400

    Fix image optimization error by disabling it in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build_new',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
