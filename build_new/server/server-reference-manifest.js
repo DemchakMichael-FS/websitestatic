baf518f1b59cff9586df0b2528857b5e75ff4c6d
Your Name <example@example.com>
2025-05-26 17:20:09 -0400

    Add API Security article - Essential Best Practices for Modern Web Development

--- build_new/server/server-reference-manifest.js
+++ build_new/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IrZ905yxI3sv18R0yzEQG+l2VFj2Jk7rbb0ZHfDZIpA=\"\n}"
